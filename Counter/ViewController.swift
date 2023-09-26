8a7cce9ab6e1b0c3ab3efddfbacf353ceb313848
Moto-Toma <moto-toma@yandex.ru>
2023-09-26 16:37:14 +0300

    Counter working

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var labelMeaning: UILabel!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    var counter: Int = 0 {
+        didSet {
+            counterLabel.text = "\(counter)"
+        }
+    }
+    @IBOutlet weak var counterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    counter = 0
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+    }
 }
 
